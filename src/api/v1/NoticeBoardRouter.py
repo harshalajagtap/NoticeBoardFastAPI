ba39963e6bbd056481b243e4ef434c87f7b58af7
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-17 18:30:40 +0530

    crud operation implimented without db

--- src/api/v1/NoticeBoardRouter.py
+++ src/api/v1/NoticeBoardRouter.py
@@ -1,11 +1,22 @@
 from fastapi import APIRouter
 
+
 nb_router = APIRouter()
 
 
 @nb_router.get("/notice")
-def myname(a, b=0):
-    return int(a)+int(b)
+def get_notice(rdate):
+    return {"date": rdate, "notice": "This is first notice"}
+
+
+@nb_router.post("/notice")
+def add_notice(rdate,applicable_for, title, notice):
+    return {"date": rdate, "applicable_for": applicable_for, "title": title, "notice":notice }
+
+
+@nb_router.delete("/notice")
+def delete_notice(notice_id):
+    return {"notice_id": f"notice {notice_id} has been deleted"}
 
 
 
