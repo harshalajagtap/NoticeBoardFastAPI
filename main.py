3f7b1784c7c2000bb47d2aec952dfb5eda306097
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-17 12:11:17 +0530

    My first API

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()         # creating instance
+
+@app.get('/blog')
+def myname(a, b=0):
+    return int(a)+int(b)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
