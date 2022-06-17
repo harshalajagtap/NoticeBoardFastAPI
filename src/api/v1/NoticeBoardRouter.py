36a5cf9d108d26ca0280c03d5504e9306de5cbf3
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-17 18:43:38 +0530

    added update operation in crud

--- src/api/v1/NoticeBoardRouter.py
+++ src/api/v1/NoticeBoardRouter.py
@@ -13,6 +13,10 @@ def get_notice(rdate):
 def add_notice(rdate,applicable_for, title, notice):
     return {"date": rdate, "applicable_for": applicable_for, "title": title, "notice":notice }
 
+@nb_router.put("/notice")
+def add_notice(notice_id, rdate, applicable_for, title, notice):
+    return {"notice_id":notice_id, "date": rdate, "applicable_for": applicable_for, "title": title, "notice":notice }
+
 
 @nb_router.delete("/notice")
 def delete_notice(notice_id):
